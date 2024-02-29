d679a07595bd800f3b73d459e67024bc06dd3a5e
gylman <gylmanbm@gmail.com>
2024-02-29 11:05:54 +0900

    successful signing of a message

--- src/App.jsx
+++ src/App.jsx
@@ -32,13 +32,45 @@ function App() {
   useEffect(() => {
     if (!signer) return;
     const getPubKey = async () => {
-      const pubKey = await signer.getPubKey();
-      console.log(pubKey);
       console.log(signer);
-      const toSignMessage = "hello world";
+      const toSignMessage = {
+        "types": {
+          "StarkNetDomain": [
+            { "name": "name", "type": "felt" },
+            { "name": "version", "type": "felt" },
+            { "name": "chainId", "type": "felt" },
+          ],
+          "Person": [
+            { "name": "name", "type": "felt" },
+            { "name": "wallet", "type": "felt" },
+          ],
+          "Mail": [
+            { "name": "from", "type": "Person" },
+            { "name": "to", "type": "Person" },
+            { "name": "contents", "type": "felt" },
+          ],
+        },
+        "primaryType": "Mail",
+        "domain": {
+          "name": "StarkNet Mail",
+          "version": "1",
+          "chainId": 1,
+        },
+        "message": {
+          "from": {
+            "name": "Cow",
+            "wallet": "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+          },
+          "to": {
+            "name": "Bob",
+            "wallet": "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
+          },
+          "contents": "Hello World",
+        },
+      };
 
-      const signedMessge = await signer.signMessage(toSignMessage);
-      console.log(signedMessge);
+      const signedMessage = await provider.signMessage(toSignMessage);
+      console.log(signedMessage);
     };
     getPubKey();
   }, [provider]);
@@ -88,3 +120,32 @@ function App() {
 }
 
 export default App;
+
+/* 
+
+  "2187141356957706744976776999527100874577338982907254937424583108635851815118",
+  "10000000000000",
+  "0"
+
+*/
+
+/* 
+
+
+  describe('Signatures', () => {
+    test('Message signature', async () => {
+      const myPrivateKey = '0x525bc68475c0955fae83869beec0996114d4bb27b28b781ed2a20ef23121b8de';
+      const myEthSigner = new EthSigner(myPrivateKey);
+      const message = typedDataExample;
+      const sig = await myEthSigner.signMessage(
+        message,
+        '0x65a822fbee1ae79e898688b5a4282dc79e0042cbed12f6169937fddb4c26641'
+      );
+      expect(sig).toMatchObject({
+        r: 46302720252787165203319064060867586811009528414735725622252684979112343882634n,
+        s: 44228007167516598548621407232357037139087111723794788802261070080184864735744n,
+        recovery: 1,
+      });
+    });
+
+*/
